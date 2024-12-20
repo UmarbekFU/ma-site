2e95f704e49687b1bf76dd8c28ed46b508aec061
Umarbek Fazliddinovich <153717176+UmarbekFU@users.noreply.github.com>
2024-12-20 10:19:13 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,28 +3,45 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
+const fs = require('fs');
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 
 // Basic test route
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Server is working!' });
 });
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB:', err));
+// MongoDB connection
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+  console.error('MONGODB_URI is not defined in environment variables');
+} else {
+  mongoose.connect(mongoUri)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
+}
 
 // Serve static files
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public'));
+  
+  app.get('*', (req, res) => {
+    try {
+      if (fs.existsSync(path.join(__dirname, 'public', 'index.html'))) {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+      } else {
+        res.send('Application is running but index.html is not found');
+      }
+    } catch (err) {
+      res.send('Error serving static files');
+    }
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
